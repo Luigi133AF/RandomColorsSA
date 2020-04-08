702d992c1abf44122e447e8b29181ece9281217a
Grzegorz Wojcik <Gzwojcik@icloud.com>
2020-04-08 15:53:55 +0200

    Initial Commit

---
+++ RandomColorsSA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomColorsSA
+//
+//  Created by zgaga on 08/04/2020.
+//  Copyright © 2020 Hime Electronics Int. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
